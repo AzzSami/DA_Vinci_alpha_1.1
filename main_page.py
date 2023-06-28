516aca8e744c3ded50d040e104929e587ba192ef
Konni <94867957+AzzSami@users.noreply.github.com>
2023-06-28 11:06:27 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -79,8 +79,10 @@ def gather_data():
     funct_gather_data()
     
 def baseline_model(): 
-    
-    funct_baseline_model()
+    if data == None :
+            st.error('Please click on the Gather Data section to choose a stock')
+    else :
+            funct_baseline_model()
     
 def random_walk(): 
     
